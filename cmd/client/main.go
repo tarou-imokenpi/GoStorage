b4fa570cfa8d1e1887b7a44e94ca28e8ff37d7a9
tarou-imokenpi <91922753+tarou-imokenpi@users.noreply.github.com>
2024-11-16 12:28:50 +0900

    newUploadFileを追加

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,79 @@
 package main
 
-func main() {
+import (
+	storage "CloudStorage/pkg/grpc/storage/proto"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
 
+var clinet storage.StorageServiceClient
+
+func main() {
+	address := "localhost:8080"
+
+	// セキュリティを無効にして接続
+	opt := grpc.WithTransportCredentials(insecure.NewCredentials())
+	conn, err := grpc.NewClient(address, opt)
+	if err != nil {
+		log.Fatal("connection error: ", err)
+		return
+	}
+	defer conn.Close()
+
+	clinet := storage.NewStorageServiceClient(conn)
+
+}
+
+func NewUploadFile(data []byte) error {
+	// streamを生成
+	stream, err := clinet.NewUploadFile(context.Background())
+	if err != nil {
+		log.Fatal("upload file error: ", err)
+		return err
+	}
+
+	// リクエストを生成
+	req := &storage.NewUploadFileRequest{
+		Meta: &storage.FileMeta{
+			Id:       "",
+			Filename: "",
+			Path:     "",
+			Size:     uint64(len(data)),
+		},
+		Data: data,
+	}
+
+	size := len(data)
+
+	sendCount := size / (1024 * 1024)
+	if size%(1024*1024) == 0 {
+		if err := stream.Send(req); err != nil {
+			log.Fatal("send error: ", err)
+			return err
+		}
+
+	} else {
+		// 1MBずつ送信
+		// 1MB未満の場合は1回で送信
+		for i := 0; i < sendCount; i++ {
+			req.Data = data[i*1024*1024 : (i+1)*1024*1024]
+			err := stream.Send(req)
+			if err != nil {
+				log.Fatal("send error: ", err)
+				return err
+			}
+		}
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatal("close and recv error: ", err)
+		return err
+	}
+
+	fmt.Println("upload file response: ", res.GetSuccess())
+	return nil
 }
